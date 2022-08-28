20df8e2f72f8eb596087df9b3aefd8894fb98e9d
LeonardoDacosta29 <dacostaleonardo137@gmail.com>
2022-08-28 20:00:11 +0700

    membuat objek dan fungsi update dan hapus pad laya

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,1 +1,17 @@
-console.log("Berhasil menggunakan JavaScript");
+const calculator = {
+  displayNumber: "0",
+  operartor: null,
+  firstNumber: null,
+  isWaitForSecondNumber: false,
+};
+
+function updateDisplay() {
+  document.querySelector("#displaynumber").innertext = calculator.displayNumber;
+}
+
+function clearCalculator() {
+  calculator.displayNumber = "0";
+  calculator.operartor = null;
+  calculator.firstNumber = null;
+  calculator.isWaitForSecondNumber = false;
+}
---
+++ ngetesjs.js
@@ -0,0 +1,7 @@
+<button onclick="alert('Anda menekan elemen button!')">Click di sini!</button>
+/* alert("Terimakasih.");
+let x; // x merupakan undefined
+x = 1 // sekarang x merupakan number
+x = true // sekarang x merupakan boolean
+x = "Harry" // sekarang x merupakan string
+*/
