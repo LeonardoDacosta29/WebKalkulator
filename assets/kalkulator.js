ee1d76bd7aa1e7ea7c3cc8c7ca5bcf8d2e0dbdd6
LeonardoDacosta29 <dacostaleonardo137@gmail.com>
2022-08-28 20:13:28 +0700

    membuat variable button dan tambah even clik

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,10 +1,11 @@
+// objek
 const calculator = {
   displayNumber: "0",
   operartor: null,
   firstNumber: null,
   isWaitForSecondNumber: false,
 };
-
+// fungsi
 function updateDisplay() {
   document.querySelector("#displaynumber").innertext = calculator.displayNumber;
 }
@@ -14,4 +15,21 @@ function clearCalculator() {
   calculator.operartor = null;
   calculator.firstNumber = null;
   calculator.isWaitForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+  calculator.displayNumber += digit;
+}
+
+// variable
+const buttons = document.querySelectorAll(".button");
+
+for (const button of buttons) {
+  button.addEventListener("click", function (event) {
+    // mendapatkan objek elemen yang dilkik
+    const target = event.target;
+
+    inputDigit(target.innertext);
+    updateDisplay();
+  });
 }
